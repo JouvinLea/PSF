ee5cf6f987c67a92d4cb01d70fe191b41d8089d4
Lea Jouvin <lea.jouvin@gmail.com>
2016-02-03 10:24:45 +0100

    pour chaque eff, off et zenith on ecrit dans un pdf tous les plots pour chaque energie

--- PSFtable.py
+++ PSFtable.py
@@ -7,8 +7,9 @@ import FrenchMcBands
 import PSFfit
 from scipy.special import erf
 from astropy.stats import poisson_conf_interval
-plt.ion()
+#plt.ion()
 from scipy import stats
+from matplotlib.backends.backend_pdf import PdfPages
 
 """
 Fonction defenition
@@ -114,7 +115,7 @@ def plot_fit_delchi(x,data,err,model_fun,save_fig):
     plt.subplots_adjust(hspace=0.1)
     plt.savefig(save_fig)
 
-def plot_fit_delchi_int(x,data,err,model_fun,save_fig, run_number, MC_band, eff):
+def plot_fit_delchi_int(x,data,err,model_fun,save_fig, run_number, MC_band, eff, pdf):
     E=MCband.ener_MC(run_number)
     zen=MCband.zen_MC(run_number)
     off=MCband.off_MC(run_number)
@@ -137,7 +138,8 @@ def plot_fit_delchi_int(x,data,err,model_fun,save_fig, run_number, MC_band, eff)
     resid = (data - model_fun)/err
     ax2.errorbar(x, resid, yerr=np.ones_like(x), fmt='o', color='k')
     plt.subplots_adjust(hspace=0.1)
-    plt.savefig(save_fig)
+    #plt.savefig(save_fig)
+    pdf.savefig()
     
 def plot_fit_delchi_test(x,data,err,model_fun,save_fig):
     fig = plt.figure()
@@ -186,15 +188,15 @@ theta2max=0.3
 MC energy, zenithal angle, offset and efficiency
 """
 #enMC = [0.02, 0.03, 0.05, 0.08, 0.125, 0.2, 0.3, 0.5, 0.8, 1.25, 2, 3, 5, 8, 12.5, 20, 30, 50, 80, 125]
-#enMC = [0.08, 0.125, 0.2, 0.3, 0.5, 0.8, 1.25, 2, 3, 5, 8, 12.5, 20, 30, 50, 80, 125]
+enMC = [0.08, 0.125, 0.2, 0.3, 0.5, 0.8, 1.25, 2, 3, 5, 8, 12.5, 20, 30, 50, 80, 125]
 #enMC = [0.08, 0.5, 0.8,0.125, 1.25, 80, 125]
-enMC = [0.125]
+#enMC = [0.125]
 #lnenMC = np.log10(enMC)
 #zenMC = [0, 18, 26, 32, 37, 41, 46, 50, 53, 57, 60, 63, 67, 70]
 #effMC = [50, 60, 70, 80, 90, 100]
 #offMC = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5]
 #zenMC = [0, 18]
-zenMC = [0]
+zenMC = [0, 18]
 effMC = [100]
 offMC = [1.0]
 
@@ -219,7 +221,7 @@ config="elm_south_stereo"
 file_nosimu = open("file_nosimu.txt", "w")
 file_toofewevents = open("file_toofewevents.txt", "w")
 file_nosimu.write("Runnumber \t E(Tev) \t Zen \t theta \t Eff \n")
-file_toofewevents.write("Runnumber \t E(Tev) \t Zen \t theta \t Eff \t Number of events \n") 
+file_toofewevents.write("Runnumber \t E(Tev) \t Zen \t theta \t Eff \t Number of events \n")
 for (ieff, eff) in enumerate(effMC):
         print eff
         for (ioff, off) in enumerate(offMC):
@@ -231,89 +233,90 @@ for (ieff, eff) in enumerate(effMC):
                 s3_init=0.08
                 A2_init=0.3
                 A3_init=0.1
-                for (ien, E) in enumerate(enMC):
-                    run_number=MCband.run_number(zen, off, E)
-                    print run_number
-                    #PSFfile=directory+"/run_"+run_number+"_"+config+"_Prod15_4_eventlist.fits"
-                    PSFfile=directory+"/run_"+run_number+"_Eff"+str(eff)+"_psf.fits"
-                    try: 
-                        hdu=pf.open(PSFfile)
+                with PdfPages('plot2/multipage_pdf'+str(zen)+'.pdf') as pdf:
+                    for (ien, E) in enumerate(enMC):
+                        run_number=MCband.run_number(zen, off, E)
+                        print run_number
+                        #PSFfile=directory+"/run_"+run_number+"_"+config+"_Prod15_4_eventlist.fits"
+                        PSFfile=directory+"/run_"+run_number+"_Eff"+str(eff)+"_psf.fits"
+                        try: 
+                            hdu=pf.open(PSFfile)
                         
-                    except:
-                        print("Cannot open file: " + PSFfile)
-                        print("skipping run")
-                        E=MCband.ener_MC(run_number)
-                        zen=MCband.zen_MC(run_number)
-                        off=MCband.off_MC(run_number)
-                        file_nosimu.write(run_number+"\t"+str(E)+"\t"+str(zen)+"\t"+str(off)+"\t"+str(eff)+"\n") 
-                        continue
+                        except:
+                            print("Cannot open file: " + PSFfile)
+                            print("skipping run")
+                            E=MCband.ener_MC(run_number)
+                            zen=MCband.zen_MC(run_number)
+                            off=MCband.off_MC(run_number)
+                            file_nosimu.write(run_number+"\t"+str(E)+"\t"+str(zen)+"\t"+str(off)+"\t"+str(eff)+"\n") 
+                            continue
                     
-                    #print hdu[1].header["MUONEFF"]
-                    #print int(hdu[4].header["OBSZEN"])
-                    #print hdu[4].header["HIERARCH TARGETOFFSET"]
-                    #print str("%.2f"%hdu[4].data["E_MIN"])
-                    theta2 = hdu[1].data["MC_ThSq"]
-                    index = [theta2<theta2max]
-                    theta2f = theta2[index]
-                    if(len(theta2f)<20):
-                        E=MCband.ener_MC(run_number)
-                        zen=MCband.zen_MC(run_number)
-                        off=MCband.off_MC(run_number)
-                        file_toofewevents.write(run_number+"\t"+str(E)+"\t"+str(zen)+"\t"+str(off)+"\t"+str(eff)+"\t"+str(len(theta2f))+"\n") 
-                        continue
-                    Nev_perbin=10
-                    Nbinmax=50
-                    theta2hist=theta2_bin(theta2f, Nev_perbin, Nbinmax)
-                    hist, bin_edges = np.histogram(theta2,theta2hist)
-                    #Me renvois la valeur moyenne en theta2 des evenements stockes dans les bins donc peut etre un peu mieux que de prendre thetabi=(Emin+Emax)/2
-                    theta2bintest, bin_edgestest,a = stats.binned_statistic(theta2,theta2,'mean',theta2hist)
-                    histtest, bin_edgestest,b = stats.binned_statistic(theta2,theta2,'count',theta2hist)
-                    PSF=PSFfit.PSFfit(theta2f)
-                    s1,s2,s3,A2,A3=PSF.minimization("triplegauss",s1_init, s2_init, s3_init, A2_init, A3_init)
-                    #s1_init=s1
-                    #s2_init=s2
-                    #s3_init=s3
-                    #A2_init=A2
-                    #A3_init=A3
-                    #sig,gam=PSF.minimization("king")
-                    TableSigma1[ien, ioff, izen, ieff] = s1
-                    TableSigma2[ien, ioff, izen, ieff] = s2
-                    TableSigma3[ien, ioff, izen, ieff] = s3
-                    TableA2[ien, ioff, izen, ieff] = A2
-                    TableA3[ien, ioff, izen, ieff] = A3
-                    #TableSig[ien, ioff, izen, ieff] = sig
-                    #TableGam[ien, ioff, izen, ieff] = gam
-                    np.savez("PSF_triplegauss_"+config+".npz", TableSigma1=TableSigma1, TableSigma2=TableSigma2, TableSigma3=TableSigma3, TableA2=TableA2, TableA3=TableA3)
-                    #np.savez("PSF_king_"+config+".npz", TableSig=TableSig, TableGam=TableGam)  
-                    #If the energy bin are in log, we have to take sqrt(Emin*Emax) for the center of the bin
-                    #theta2bin = np.sqrt(bin_edges[:-1] * bin_edges[1:])
-                    theta2bin = (bin_edges[:-1] + bin_edges[1:])/2.
-                    #thetafit=np.logspace(np.log10(theta2bin[0]),np.log10(theta2bin[-1]),100)
-                    #We have to divide by the solid angle of each bin= pi*dO^2 to normalize the histogram
-                    bsize = np.diff(bin_edges)*math.pi
-                    hist_norm = hist/float(np.sum(hist))/bsize
-                    bsizetest = np.diff(bin_edgestest)*math.pi
-                    hist_normtest = histtest/float(np.sum(histtest))/bsizetest
-                    # use gehrels errors for low counts (http://cxc.harvard.edu/sherpa4.4/statistics/)
-                    hist_err = (1+np.sqrt(hist+0.75))/float(np.sum(hist))/bsize
-                    hist_err2 = (1+np.sqrt(histtest+0.75))/float(np.sum(histtest))/bsizetest
-                    #Erreur prenant en compte poisson du coup j ai des erreurs asymetrics inf et sup
-                    histerr_test=poisson_conf_interval(hist)/float(np.sum(hist))/bsize
-                    #fitgauss = lambda x : triplegauss(x,s1,s2,s3,A2,A3)
-                    Int_fitgauss = lambda x1,x2 : Integral_triplegauss(x1,x2,s1,s2,s3,A2,A3)
+                        #print hdu[1].header["MUONEFF"]
+                        #print int(hdu[4].header["OBSZEN"])
+                        #print hdu[4].header["HIERARCH TARGETOFFSET"]
+                        #print str("%.2f"%hdu[4].data["E_MIN"])
+                        theta2 = hdu[1].data["MC_ThSq"]
+                        index = [theta2<theta2max]
+                        theta2f = theta2[index]
+                        if(len(theta2f)<20):
+                            E=MCband.ener_MC(run_number)
+                            zen=MCband.zen_MC(run_number)
+                            off=MCband.off_MC(run_number)
+                            file_toofewevents.write(run_number+"\t"+str(E)+"\t"+str(zen)+"\t"+str(off)+"\t"+str(eff)+"\t"+str(len(theta2f))+"\n") 
+                            continue
+                        Nev_perbin=10
+                        Nbinmax=50
+                        theta2hist=theta2_bin(theta2f, Nev_perbin, Nbinmax)
+                        hist, bin_edges = np.histogram(theta2,theta2hist)
+                        #Me renvois la valeur moyenne en theta2 des evenements stockes dans les bins donc peut etre un peu mieux que de prendre thetabi=(Emin+Emax)/2
+                        theta2bintest, bin_edgestest,a = stats.binned_statistic(theta2,theta2,'mean',theta2hist)
+                        histtest, bin_edgestest,b = stats.binned_statistic(theta2,theta2,'count',theta2hist)
+                        PSF=PSFfit.PSFfit(theta2f)
+                        s1,s2,s3,A2,A3=PSF.minimization("triplegauss",s1_init, s2_init, s3_init, A2_init, A3_init)
+                        s1_init=s1
+                        s2_init=s2
+                        s3_init=s3
+                        A2_init=A2
+                        A3_init=A3
+                        #sig,gam=PSF.minimization("king")
+                        TableSigma1[ien, ioff, izen, ieff] = s1
+                        TableSigma2[ien, ioff, izen, ieff] = s2
+                        TableSigma3[ien, ioff, izen, ieff] = s3
+                        TableA2[ien, ioff, izen, ieff] = A2
+                        TableA3[ien, ioff, izen, ieff] = A3
+                        #TableSig[ien, ioff, izen, ieff] = sig
+                        #TableGam[ien, ioff, izen, ieff] = gam
+                        np.savez("PSF_triplegauss_"+config+".npz", TableSigma1=TableSigma1, TableSigma2=TableSigma2, TableSigma3=TableSigma3, TableA2=TableA2, TableA3=TableA3)
+                        #np.savez("PSF_king_"+config+".npz", TableSig=TableSig, TableGam=TableGam)  
+                        #If the energy bin are in log, we have to take sqrt(Emin*Emax) for the center of the bin
+                        #theta2bin = np.sqrt(bin_edges[:-1] * bin_edges[1:])
+                        theta2bin = (bin_edges[:-1] + bin_edges[1:])/2.
+                        #thetafit=np.logspace(np.log10(theta2bin[0]),np.log10(theta2bin[-1]),100)
+                        #We have to divide by the solid angle of each bin= pi*dO^2 to normalize the histogram
+                        bsize = np.diff(bin_edges)*math.pi
+                        hist_norm = hist/float(np.sum(hist))/bsize
+                        bsizetest = np.diff(bin_edgestest)*math.pi
+                        hist_normtest = histtest/float(np.sum(histtest))/bsizetest
+                        # use gehrels errors for low counts (http://cxc.harvard.edu/sherpa4.4/statistics/)
+                        hist_err = (1+np.sqrt(hist+0.75))/float(np.sum(hist))/bsize
+                        hist_err2 = (1+np.sqrt(histtest+0.75))/float(np.sum(histtest))/bsizetest
+                        #Erreur prenant en compte poisson du coup j ai des erreurs asymetrics inf et sup
+                        histerr_test=poisson_conf_interval(hist)/float(np.sum(hist))/bsize
+                        #fitgauss = lambda x : triplegauss(x,s1,s2,s3,A2,A3)
+                        Int_fitgauss = lambda x1,x2 : Integral_triplegauss(x1,x2,s1,s2,s3,A2,A3)
                     
-                    #fitking = lambda x : king(x,sig,gam)
-                    #save_fig="plot2/triplegauss_fitspsf_run_"+run_number+"_eff_"+str(eff)+".jpg"
-                    #plot_fit_delchi(theta2bin,hist_norm,hist_err,fitgauss,save_fig)
-                    save_fig_int="plot2/INT_triplegauss_fitspsf_run_"+run_number+"_eff_"+str(eff)+".jpg"
-                    plot_fit_delchi_int(theta2bin,hist_norm,hist_err,Int_fitgauss (bin_edges[:-1],bin_edges[1:])/(bsize) ,save_fig_int, run_number, MCband, eff)
-                    #save_fig_scipy="plot/triplegauss_fitspsf_run_"+run_number+".jpg"
-                    #plot_fit_delchi(theta2bintest,hist_normtest,hist_err2,fitgauss,save_fig_scipy,5)
-                    #save_fig3="plot/triplegauss_poissonianerror_fitspsf_run_"+run_number+".jpg"
-                    #plot_fit_delchi_test(theta2bin,hist_norm,histerr_test,fitgauss,save_fig3,5)
-                    #save_fig2="plot/king_fitspsf_run_"+run_number+".jpg"
-                    #plot_fit_delchi(theta2bin,hist_norm,hist_err,fitking,save_fig2, 2)
-                    #test_gauss=bin_contigu(theta2bin,hist_norm,fitgauss, 1/3.)
-                    #test_king=bin_contigu(theta2bin,hist_norm,fitking, 1/3.)
+                        #fitking = lambda x : king(x,sig,gam)
+                        #save_fig="plot2/triplegauss_fitspsf_run_"+run_number+"_eff_"+str(eff)+".jpg"
+                        #plot_fit_delchi(theta2bin,hist_norm,hist_err,fitgauss,save_fig)
+                        save_fig_int="plot2/INT_triplegauss_fitspsf_run_"+run_number+"_eff_"+str(eff)+".jpg"
+                        plot_fit_delchi_int(theta2bin,hist_norm,hist_err,Int_fitgauss (bin_edges[:-1],bin_edges[1:])/(bsize) ,save_fig_int, run_number, MCband, eff, pdf)
+                        #save_fig_scipy="plot/triplegauss_fitspsf_run_"+run_number+".jpg"
+                        #plot_fit_delchi(theta2bintest,hist_normtest,hist_err2,fitgauss,save_fig_scipy,5)
+                        #save_fig3="plot/triplegauss_poissonianerror_fitspsf_run_"+run_number+".jpg"
+                        #plot_fit_delchi_test(theta2bin,hist_norm,histerr_test,fitgauss,save_fig3,5)
+                        #save_fig2="plot/king_fitspsf_run_"+run_number+".jpg"
+                        #plot_fit_delchi(theta2bin,hist_norm,hist_err,fitking,save_fig2, 2)
+                        #test_gauss=bin_contigu(theta2bin,hist_norm,fitgauss, 1/3.)
+                        #test_king=bin_contigu(theta2bin,hist_norm,fitking, 1/3.)
 file_toofewevents.close()
 file_nosimu.close()
