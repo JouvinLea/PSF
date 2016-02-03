e4d545bcd7d72e88932cd7193022f6b175909fa5
Lea Jouvin <lea.jouvin@gmail.com>
2016-02-03 12:48:03 +0100

    ajout du R68

--- PSFtable.py
+++ PSFtable.py
@@ -41,7 +41,38 @@ def Integral_triplegauss(theta2min,theta2max,s1,s2,s3,A2,A3):
     norm =  2*math.pi*(s12+ np.abs(A2) * s22 + np.abs(A3) * s32)
     return y/norm
 
+def eff_sigma(s1,s2,s3,A2,A3):
+    s12 = s1*s1
+    s22 = s2*s2
+    s32 = s3*s3
+    theta=np.sqrt(s12+ np.abs(A2) * s22 + np.abs(A3) * s32)
+    return theta
 
+def containment_fraction_singlegauss(theta,sigma):
+    theta = np.asarray(theta, dtype=np.float64)
+
+    return 1 - np.exp(-0.5 * theta ** 2 / sigma**2)
+
+def containment_fraction_tripplegauss(theta,s1,s2,s3,A2,A3):
+    theta = np.asarray(theta, dtype=np.float64)
+    f1=containment_fraction_singlegauss(theta,s1)
+    f2=containment_fraction_singlegauss(theta,s2)
+    f3=containment_fraction_singlegauss(theta,s3)
+    tot=f1+A2*f2+A3*f3
+    return tot
+
+def containment_radius_tripplegauss(containment_fraction,s1,s2,s3,A2,A3):
+    from scipy.optimize import brentq
+    def f(theta):
+        # positive if theta too large
+        return containment_fraction_tripplegauss(theta,s1,s2,s3,A2,A3) - containment_fraction
+    theta_max = eff_sigma(s1,s2,s3,A2,A3)
+        # Expand until we really find a theta_max
+    while f(theta_max) < 0:
+        theta_max *= 2
+    return brentq(f, a=0, b=theta_max)
+                
+    
 def king(theta2,sig, gam):
     norm = (1/(2*np.pi*sig**2))*(1-1/gam)
     king=(1+theta2/(2*gam*(sig**2)))**(-gam)
@@ -115,10 +146,7 @@ def plot_fit_delchi(x,data,err,model_fun,save_fig):
     plt.subplots_adjust(hspace=0.1)
     plt.savefig(save_fig)
 
-def plot_fit_delchi_int(x,data,err,model_fun,save_fig, run_number, MC_band, eff, pdf):
-    E=MCband.ener_MC(run_number)
-    zen=MCband.zen_MC(run_number)
-    off=MCband.off_MC(run_number)
+def plot_fit_delchi_int(x,data,err,model_fun,save_fig, E, zen, off, eff, pdf):
     fig = plt.figure()
     gs = gridspec.GridSpec(4, 1)
     ax1 = fig.add_subplot(gs[:3,:]) # rows, cols, plot_num.
@@ -141,27 +169,23 @@ def plot_fit_delchi_int(x,data,err,model_fun,save_fig, run_number, MC_band, eff,
     #plt.savefig(save_fig)
     pdf.savefig()
     
-def plot_fit_delchi_test(x,data,err,model_fun,save_fig):
+def plot_khi2(E ,khi2, pdf):
     fig = plt.figure()
-    gs = gridspec.GridSpec(4, 1)
-    ax1 = fig.add_subplot(gs[:3,:]) # rows, cols, plot_num.
-    ax1.set_xscale("log", nonposx='clip')
-    ax1.set_yscale("log", nonposy='clip')
+    plt.plot(E, khi2, "o")
+    plt.ylabel("khi2")
+    plt.xlabel("E (TeV)")
+    plt.axhline(y=2, color='red',linewidth=4)
+    plt.title("khi2 evolution with MC energy")
+    pdf.savefig()
 
-    ax1.errorbar(x,data,yerr=err,fmt='o',color='k')
-    xmod = np.linspace(np.min(x),np.max(x),10000)
-    KHI2=khi2(x, data, err, model_fun, N_param_model)
-    line1,=ax1.plot(x,model_fun(x))
-    ax1.plot(x,model_fun(x))
-    ax1.get_xaxis().set_visible(False)
-    plt.legend([line1], ["khi2= "+str("%.2f"%KHI2)])
-
-    #ax2 = fig.add_subplot(gs[3,:],sharex=ax1) 
-    #ax2.plot(xmod,np.zeros_like(xmod),color='k')
-    #resid = (data - model_fun(x))/err
-    #ax2.errorbar(x, resid, yerr=np.ones_like(x), fmt='o', color='k')
-    #plt.subplots_adjust(hspace=0.1)
-    plt.savefig(save_fig)
+def plot_R68(E , R68, pdf):
+    fig = plt.figure()
+    plt.plot(E, R68, "o")
+    plt.ylabel("R68")
+    plt.xlabel("E (TeV)")
+    plt.title("R68 evolution with MC energy")
+    pdf.savefig()
+       
     
 def theta2_bin(data, Nev_bin, Nbinmax):
     """
@@ -188,15 +212,15 @@ theta2max=0.3
 MC energy, zenithal angle, offset and efficiency
 """
 #enMC = [0.02, 0.03, 0.05, 0.08, 0.125, 0.2, 0.3, 0.5, 0.8, 1.25, 2, 3, 5, 8, 12.5, 20, 30, 50, 80, 125]
-enMC = [0.08, 0.125, 0.2, 0.3, 0.5, 0.8, 1.25, 2, 3, 5, 8, 12.5, 20, 30, 50, 80, 125]
-#enMC = [0.08, 0.5, 0.8,0.125, 1.25, 80, 125]
+#enMC = [0.08, 0.125, 0.2, 0.3, 0.5, 0.8, 1.25, 2, 3, 5, 8, 12.5, 20, 30, 50, 80, 125]
+enMC = [0.08, 0.5, 0.8,0.125, 1.25, 80, 125]
 #enMC = [0.125]
 #lnenMC = np.log10(enMC)
 #zenMC = [0, 18, 26, 32, 37, 41, 46, 50, 53, 57, 60, 63, 67, 70]
 #effMC = [50, 60, 70, 80, 90, 100]
 #offMC = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5]
-#zenMC = [0, 18]
 zenMC = [0, 18]
+#zenMC = [0]
 effMC = [100]
 offMC = [1.0]
 
@@ -220,8 +244,10 @@ directory="/Users/jouvin/Desktop/these/WorkGAMMAPI/IRF/PSF"
 config="elm_south_stereo"
 file_nosimu = open("file_nosimu.txt", "w")
 file_toofewevents = open("file_toofewevents.txt", "w")
+file_khi2toohigh = open("file_khi2sup2.txt", "w")
 file_nosimu.write("Runnumber \t E(Tev) \t Zen \t theta \t Eff \n")
 file_toofewevents.write("Runnumber \t E(Tev) \t Zen \t theta \t Eff \t Number of events \n")
+file_khi2toohigh.write("Runnumber \t E(Tev) \t Zen \t theta \t Eff \t Number of events \t khi2 \n")
 for (ieff, eff) in enumerate(effMC):
         print eff
         for (ioff, off) in enumerate(offMC):
@@ -233,7 +259,10 @@ for (ieff, eff) in enumerate(effMC):
                 s3_init=0.08
                 A2_init=0.3
                 A3_init=0.1
-                with PdfPages('plot2/multipage_pdf'+str(zen)+'.pdf') as pdf:
+                khi2_list=[]
+                Eok_list=[]
+                R68_list=[]
+                with PdfPages('plot2/zen_'+str(zen)+'_eff_'+str(eff)+'_off_'+str(off)+'.pdf') as pdf:
                     for (ien, E) in enumerate(enMC):
                         run_number=MCband.run_number(zen, off, E)
                         print run_number
@@ -245,9 +274,6 @@ for (ieff, eff) in enumerate(effMC):
                         except:
                             print("Cannot open file: " + PSFfile)
                             print("skipping run")
-                            E=MCband.ener_MC(run_number)
-                            zen=MCband.zen_MC(run_number)
-                            off=MCband.off_MC(run_number)
                             file_nosimu.write(run_number+"\t"+str(E)+"\t"+str(zen)+"\t"+str(off)+"\t"+str(eff)+"\n") 
                             continue
                     
@@ -259,9 +285,6 @@ for (ieff, eff) in enumerate(effMC):
                         index = [theta2<theta2max]
                         theta2f = theta2[index]
                         if(len(theta2f)<20):
-                            E=MCband.ener_MC(run_number)
-                            zen=MCband.zen_MC(run_number)
-                            off=MCband.off_MC(run_number)
                             file_toofewevents.write(run_number+"\t"+str(E)+"\t"+str(zen)+"\t"+str(off)+"\t"+str(eff)+"\t"+str(len(theta2f))+"\n") 
                             continue
                         Nev_perbin=10
@@ -309,7 +332,16 @@ for (ieff, eff) in enumerate(effMC):
                         #save_fig="plot2/triplegauss_fitspsf_run_"+run_number+"_eff_"+str(eff)+".jpg"
                         #plot_fit_delchi(theta2bin,hist_norm,hist_err,fitgauss,save_fig)
                         save_fig_int="plot2/INT_triplegauss_fitspsf_run_"+run_number+"_eff_"+str(eff)+".jpg"
-                        plot_fit_delchi_int(theta2bin,hist_norm,hist_err,Int_fitgauss (bin_edges[:-1],bin_edges[1:])/(bsize) ,save_fig_int, run_number, MCband, eff, pdf)
+                        plot_fit_delchi_int(theta2bin,hist_norm,hist_err,Int_fitgauss (bin_edges[:-1],bin_edges[1:])/(bsize) ,save_fig_int, E, zen, off, eff, pdf)
+                        KHI2=khi2_int(theta2bin,hist_norm,hist_err,Int_fitgauss (bin_edges[:-1],bin_edges[1:])/(bsize))
+                        if(KHI2 > 2):
+                            file_khi2toohigh.write(run_number+"\t"+str(E)+"\t"+str(zen)+"\t"+str(off)+"\t"+str(eff)+"\t"+str(len(theta2f))+"\t"+str(KHI2)+"\n") 
+                        Eok_list.append(E)    
+                        khi2_list.append(KHI2)
+                        R68=containment_radius_tripplegauss(0.68,s1,s2,s3,A2,A3)
+                        R68_list.append(R68)
+                    plot_khi2(Eok_list , khi2_list, pdf)
+                    plot_R68(Eok_list , R68_list, pdf)
                         #save_fig_scipy="plot/triplegauss_fitspsf_run_"+run_number+".jpg"
                         #plot_fit_delchi(theta2bintest,hist_normtest,hist_err2,fitgauss,save_fig_scipy,5)
                         #save_fig3="plot/triplegauss_poissonianerror_fitspsf_run_"+run_number+".jpg"
